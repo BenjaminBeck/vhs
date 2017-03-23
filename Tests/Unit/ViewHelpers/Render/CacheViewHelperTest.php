4acf705328dd87f31216c96cc0d565ce5b3aedd5
Benjamin Beck <BenjaminBeck@gmx.de>
2017-03-23 17:37:33 +0100

    [WIP]

--- Tests/Unit/ViewHelpers/Render/CacheViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Render/CacheViewHelperTest.php
@@ -16,4 +16,31 @@ use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTest;
 class CacheViewHelperTest extends AbstractViewHelperTest
 {
 
+	public function testCache() {
+		$this->assertFalse(true);
+		$cacheIdentity = 'unique-cache-id';
+		$cacheContent = 'some-cache-content';
+		$renderChildrenClosure = function() use ($cacheContent) {
+			return $cacheContent;
+		};
+		/** @var \FluidTYPO3\Vhs\ViewHelpers\Render\CacheViewHelper $RenderCache */
+		$RenderCache = $this->objectManager->get('FluidTYPO3\Vhs\ViewHelpers\Render\CacheViewHelper');
+
+		$cacheObject = $this->getMock('\TYPO3\CMS\Core\Cache\Frontend\StringFrontend', array('isValidEntryIdentifier', 'has', 'set', 'get'));
+
+		$RenderCache->setRenderChildrenClosure($renderChildrenClosure);
+		$arguments = array(
+			'identity' => $cacheIdentity,
+			'content'  => NULL
+		);
+		$RenderCache->setArguments($arguments);
+		$RenderCache->initialize();
+
+		$result = $RenderCache->render( $cacheIdentity);
+	}
+
+
+
+
+
 }
