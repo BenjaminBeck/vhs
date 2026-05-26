eb12398fa720aec68abd07c6ca84982ca327afcc
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-26 21:05:03 +0200

    [TASK] [0003] Use EXTENSIONS config value for VHS middleware disable flag

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -1,4 +1,7 @@
 <?php
+
+$disableAssetHandling = (bool) ($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vhs']['disableAssetHandling'] ?? false);
+
 return [
     'frontend' => [
         'fluidtypo3/vhs/asset-inclusion' => [
@@ -6,7 +9,7 @@ return [
             'after' => [
                 'typo3/cms-frontend/content-length-headers',
             ],
-            'disabled' => filter_var($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['vhs']['setup']['disableAssetHandling'] ?? false, \FILTER_VALIDATE_BOOL),
+            'disabled' => $disableAssetHandling,
         ],
         'fluidtypo3/vhs/request-availability' => [
             'target' => \FluidTYPO3\Vhs\Middleware\RequestAvailability::class,
