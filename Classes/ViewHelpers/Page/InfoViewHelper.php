a294665609c5826b83dcd51873ffb0fece07bf08
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-26 22:36:36 +0200

    [BUGFIX] 0214 Resolve current page uid from request routing/TSFE fallback

--- Classes/ViewHelpers/Page/InfoViewHelper.php
+++ Classes/ViewHelpers/Page/InfoViewHelper.php
@@ -12,8 +12,10 @@ use FluidTYPO3\Vhs\Service\PageService;
 use FluidTYPO3\Vhs\Traits\CompileWithRenderStatic;
 use FluidTYPO3\Vhs\Traits\TemplateVariableViewHelperTrait;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Routing\PageArguments;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use FluidTYPO3\Vhs\Core\ViewHelper\AbstractViewHelper;
+use FluidTYPO3\Vhs\Utility\RequestResolver;
 
 /**
  * ViewHelper to access data of the current page record.
@@ -59,9 +61,22 @@ class InfoViewHelper extends AbstractViewHelper
         $pageService = GeneralUtility::makeInstance(PageService::class);
         $pageRepository = $pageService->getPageRepository();
         /** @var int $pageUid */
-        $pageUid = $arguments['pageUid'];
+        $pageUid = (int) ($arguments['pageUid'] ?? 0);
         if (0 === $pageUid) {
-            $pageUid = $GLOBALS['TSFE']->id;
+            try {
+                $request = RequestResolver::resolveRequestFromRenderingContext($renderingContext);
+            } catch (\UnexpectedValueException) {
+                $request = null;
+            }
+            if ($request instanceof \Psr\Http\Message\ServerRequestInterface && $request->getAttribute('routing') instanceof PageArguments) {
+                $pageUid = (int) $request->getAttribute('routing')->getPageId();
+            } elseif (isset($GLOBALS['TSFE']) && is_object($GLOBALS['TSFE']) && property_exists($GLOBALS['TSFE'], 'id')) {
+                $pageUid = (int) $GLOBALS['TSFE']->id;
+            } else {
+                $rootLine = $pageService->getRootLine();
+                $page = $rootLine[0] ?? [];
+                $pageUid = (int) ($page['uid'] ?? 0);
+            }
         }
         $page = $pageRepository->getPage_noCheck((int) $pageUid);
         /** @var string|null $field */
