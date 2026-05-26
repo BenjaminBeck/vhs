8d05d740657c3eb136772dcf053521187fd62dd6
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-26 22:06:23 +0200

    [BUGFIX] [0256] Resolve request URL from request attributes in Uri request viewhelper

--- Tests/Unit/ViewHelpers/Uri/RequestViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Uri/RequestViewHelperTest.php
@@ -10,7 +10,8 @@ namespace FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\Uri;
 
 use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTest;
 use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTestCase;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Http\NormalizedParams;
+use TYPO3\CMS\Core\Http\ServerRequest;
 
 /**
  * Class RequestViewHelperTest
@@ -22,7 +23,17 @@ class RequestViewHelperTest extends AbstractViewHelperTestCase
      */
     public function rendersUrl()
     {
+        $expectedUrl = 'https://example.test/?foo=1';
+        $normalizedParams = $this->getMockBuilder(NormalizedParams::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $normalizedParams->method('getRequestUrl')->willReturn($expectedUrl);
+
+        $serverRequest = new ServerRequest($expectedUrl);
+        $serverRequest = $serverRequest->withAttribute('normalizedParams', $normalizedParams);
+        $GLOBALS['TYPO3_REQUEST'] = $serverRequest;
+
         $test = $this->executeViewHelper();
-        $this->assertSame(GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL'), $test);
+        $this->assertSame($expectedUrl, $test);
     }
 }
