ff64bacaf94182d5be76afb2d21a8918d9d5bd78
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-27 00:19:36 +0200

    [TASK] [0264,0499] Type Typoscript viewhelper tests

--- Tests/Unit/ViewHelpers/Variable/TyposcriptViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Variable/TyposcriptViewHelperTest.php
@@ -45,7 +45,7 @@ class TyposcriptViewHelperTest extends AbstractViewHelperTestCase
     /**
      * @test
      */
-    public function returnsNullIfPathIsNull()
+    public function returnsNullIfPathIsNull(): void
     {
         $this->assertNull($this->executeViewHelper(['path' => null]));
     }
@@ -53,7 +53,7 @@ class TyposcriptViewHelperTest extends AbstractViewHelperTestCase
     /**
      * @test
      */
-    public function returnsArrayIfPathContainsArray()
+    public function returnsArrayIfPathContainsArray(): void
     {
         $this->assertThat($this->executeViewHelper(['path' => 'config.tx_extbase.features']), new IsType(IsType::TYPE_ARRAY));
     }
@@ -61,7 +61,7 @@ class TyposcriptViewHelperTest extends AbstractViewHelperTestCase
     /**
      * @test
      */
-    public function canGetPathUsingArgument()
+    public function canGetPathUsingArgument(): void
     {
         $this->assertNotEmpty($this->executeViewHelper(['path' => 'config.tx_extbase.features']));
     }
@@ -69,7 +69,7 @@ class TyposcriptViewHelperTest extends AbstractViewHelperTestCase
     /**
      * @test
      */
-    public function canGetPathUsingTagContent()
+    public function canGetPathUsingTagContent(): void
     {
         $this->assertNotEmpty($this->executeViewHelperUsingTagContent('config.tx_extbase.features'));
     }
