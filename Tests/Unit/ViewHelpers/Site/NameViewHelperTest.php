558d6c1e2fa73d99198c84861d8a1c9179d0efc8
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-27 02:20:11 +0200

    [TASK] [0245-0246] Tighten security and site helpers

--- Tests/Unit/ViewHelpers/Site/NameViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Site/NameViewHelperTest.php
@@ -10,6 +10,9 @@ namespace FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\Site;
 
 use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTest;
 use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTestCase;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 
 /**
  * Class NameViewHelperTest
@@ -19,11 +22,41 @@ class NameViewHelperTest extends AbstractViewHelperTestCase
     /**
      * @test
      */
-    public function rendersSiteName()
+    public function rendersSiteName(): void
     {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = 'test';
         $test = $this->executeViewHelper();
         unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename']);
         $this->assertSame('test', $test);
+    }
+
+    /**
+     * @test
+     */
+    public function rendersCurrentLanguageWebsiteTitle(): void
+    {
+        $language = $this->getMockBuilder(SiteLanguage::class)
+            ->onlyMethods(['getWebsiteTitle'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $language->method('getWebsiteTitle')->willReturn('Language title');
+        $GLOBALS['TYPO3_REQUEST'] = (new ServerRequest())->withAttribute('language', $language);
+
+        self::assertSame('Language title', $this->executeViewHelper());
+    }
+
+    /**
+     * @test
+     */
+    public function rendersSiteWebsiteTitle(): void
+    {
+        $site = new Site('test', 1, [
+            'base' => '/',
+            'websiteTitle' => 'Site title',
+            'languages' => [],
+        ]);
+        $GLOBALS['TYPO3_REQUEST'] = (new ServerRequest())->withAttribute('site', $site);
+
+        self::assertSame('Site title', $this->executeViewHelper());
     }
 }
