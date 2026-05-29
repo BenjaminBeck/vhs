f6ee50ae24aaf5f6d2612c73aea6367f561727fb
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2026-05-30 00:03:37 +0200

    [BUGFIX] Add CSP asset setting surface

--- Classes/Asset.php
+++ Classes/Asset.php
@@ -84,6 +84,7 @@ class Asset implements AssetInterface
     protected bool $async = false;
     protected bool $defer = false;
     protected bool $rewrite = true;
+    protected ?bool $csp = null;
     private static ?array $settingsCache = null;
 
     public function __construct()
@@ -265,6 +266,17 @@ class Asset implements AssetInterface
     public function getDefer(): bool
     {
         return $this->defer;
+    }
+
+    public function setCsp(?bool $csp): self
+    {
+        $this->csp = $csp;
+        return $this;
+    }
+
+    public function getCsp(): ?bool
+    {
+        return $this->csp;
     }
 
     public function getName(): string
